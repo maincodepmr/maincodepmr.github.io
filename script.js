9475c11dd4b06021ce7211bf29e63a710feea5a3
maincodepmr <157756498+maincodepmr@users.noreply.github.com>
2024-02-15 14:44:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function start1(){
+    let banner = document.querySelector('.banner--load');
+    banner.classList.add('banner--load');
+   
+    banner.classList.remove('banner');
+   
+}
+
+function start1(){
+  
+    setTimeout(loaded, 1000);
+}
+
+
+document.addEventListener('DOMContentLoaded', start1);
