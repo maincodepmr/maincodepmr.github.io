28863b6a3cdbccf5e1b32bb39e8d9fe86955def8
maincodepmr <157756498+maincodepmr@users.noreply.github.com>
2024-02-27 23:55:08 +0200

    projects

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function start1(){
-    let banner = document.querySelector('.banner--load');
-    banner.classList.add('banner--load');
-   
-    banner.classList.remove('banner');
-   
-}
-
-function start1(){
-  
-    setTimeout(loaded, 1000);
-}
-
-
-document.addEventListener('DOMContentLoaded', start1);
+$('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<img  class="arrow" src="Backto.png">',
+    nextArrow: '<img class="arrow" src="Nextpage.png">',
+  });
+    
+    
